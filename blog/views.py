cac5229601dabb65d7faf289bfaf34039ffe9955
nick <npalermo10@gmail.com>
2017-07-16 16:23:42 -0400

    display remainder posts

--- blog/views.py
+++ blog/views.py
@@ -6,14 +6,18 @@ def blog_index(request):
     recent_posts = Post.objects.all().order_by("posted")[:11]
     latest_post = recent_posts.reverse()[0]
     second_latest_post = recent_posts.reverse()[1]
-    older_posts = recent_posts.reverse()[2:8]
-    older_posts_grouped = zip(*[iter(older_posts)]*3)
-    
+    older_posts = recent_posts.reverse()[2:10]
+    older_posts_grouped=  zip(*[iter(older_posts)]*3)
+    if len(older_posts)%3:
+        older_posts_grouped.append(older_posts[len(older_posts_grouped)*3:])
+    even_older_posts = Post.objects.all().order_by("posted")[11:]
+
     return render_to_response('blog_index.html', {
         'categories': Category.objects.all(),
         'latest_post': latest_post,
         'second_latest_post': second_latest_post,
         'older_posts_grouped': older_posts_grouped,
+        'even_older_posts': even_older_posts,
         })
 
 def view_post(request, slug):
